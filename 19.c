6e9896ea8e27fac0ce9b0363c1434894d4c26a0e
SIDDHARTH JAIN <103947670+siddhaarthh@users.noreply.github.com>
2023-06-02 17:55:17 +0530

    19th c program

---
+++ 19.c
@@ -0,0 +1,17 @@
+// programme of swap no. using call by reference method
+#include<stdio.h>
+void swap(int *x,int *y)
+{
+    int t;
+    t=*x;
+    *x=*y;
+    *y=t;
+}
+void main()
+{
+    int a,b;
+    printf("enter a and b ");
+    scanf("%d%d",&a,&b);
+    swap(&a,&b);
+    printf("\n a=%d \n b=%d",a,b);
+}
