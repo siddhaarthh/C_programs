848b3550ead4ec04bc822a9a72d58e8501617d56
SIDDHARTH JAIN <103947670+siddhaarthh@users.noreply.github.com>
2023-06-05 17:31:40 +0530

    29th c program

---
+++ 29.c
@@ -0,0 +1,14 @@
+//pattern print
+#include<stdio.h>
+void main()
+{
+    int i,j;
+    for(i=0;i<6;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("%d",i);
+        }
+        printf("\n");
+    }
+}
