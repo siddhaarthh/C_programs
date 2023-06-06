ab9c73898bada62a6d6df5c24f00b0fd7729ccc3
SIDDHARTH JAIN <103947670+siddhaarthh@users.noreply.github.com>
2023-06-06 14:11:20 +0530

    array implementation

---
+++ array.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[9],i;
+printf("enter array ");
+for(i=0;i<9;i++)
+{
+scanf("%d",&a[i]);
+}
+printf("enter element to be searched");
+for(i=0;i<3;i++)
+{
+    printf("%d",i);
+}
+getch();
+}
