be92fe28686feed2e120c8fc74634c0f4d74df77
SIDDHARTH JAIN <103947670+siddhaarthh@users.noreply.github.com>
2023-06-09 12:52:09 +0530

    enter string

---
+++ enter_string.c
@@ -0,0 +1,13 @@
+//enter name
+#include<stdio.h>
+void main()
+{
+    char at[10],bb[10];
+    int i,n=2,j;
+    printf("enter name1 \n");
+    scanf("%s",&at);
+
+    printf("enter name2 \n");
+    scanf("%s",&bb);
+    printf("entred name is \n%s\n%s",at,bb);
+}
